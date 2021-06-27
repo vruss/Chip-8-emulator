8a2266d1b6751b8cd3cd059611f7574bb9b75ba5
Viktor Rosvall <virosen@yahoo.se>
2021-06-27 18:04:33 +0200

    Converted decoder's Method Groups into Lamdas to improve memory allocation

--- Emulator/InstructionDecoder.cs
+++ Emulator/InstructionDecoder.cs
@@ -10,55 +10,55 @@ namespace Chip_8.Emulator
 			{
 				0x0000 => (encodedInstruction & 0x00FF) switch
 				{
-					0x00E0 => Instruction.CLS,
-					0x00EE => Instruction.RET,
-					_ => Instruction.SYS_addr
+					0x00E0 => state => Instruction.CLS(state),
+					0x00EE => state => Instruction.RET(state),
+					_ => state => Instruction.SYS_addr(state)
 				},
-				0x1000 => Instruction.JP_addr,
-				0x2000 => Instruction.CALL_addr,
-				0x3000 => Instruction.SE_Vx_byte,
-				0x4000 => Instruction.SNE_Vx_byte,
-				0x5000 => Instruction.SE_Vx_Vy,
-				0x6000 => Instruction.LD_Vx_byte,
-				0x7000 => Instruction.ADD_Vx_byte,
+				0x1000 => state => Instruction.JP_addr(state),
+				0x2000 => state => Instruction.CALL_addr(state),
+				0x3000 => state => Instruction.SE_Vx_byte(state),
+				0x4000 => state => Instruction.SNE_Vx_byte(state),
+				0x5000 => state => Instruction.SE_Vx_Vy(state),
+				0x6000 => state => Instruction.LD_Vx_byte(state),
+				0x7000 => state => Instruction.ADD_Vx_byte(state),
 				0x8000 => (encodedInstruction & 0x000F) switch
 				{
-					0x0000 => Instruction.LD_Vx_Vy,
-					0x0001 => Instruction.OR_Vx_Vy,
-					0x0002 => Instruction.AND_Vx_Vy,
-					0x0003 => Instruction.XOR_Vx_Vy,
-					0x0004 => Instruction.ADD_Vx_Vy,
-					0x0005 => Instruction.SUB_Vx_Vy,
-					0x0006 => Instruction.SHR_Vx_Vy,
-					0x0007 => Instruction.SUBN_Vx_Vy,
-					0x000E => Instruction.SHL_Vx_Vy,
-					_ => Instruction.SYS_addr
+					0x0000 => state => Instruction.LD_Vx_Vy(state),
+					0x0001 => state => Instruction.OR_Vx_Vy(state),
+					0x0002 => state => Instruction.AND_Vx_Vy(state),
+					0x0003 => state => Instruction.XOR_Vx_Vy(state),
+					0x0004 => state => Instruction.ADD_Vx_Vy(state),
+					0x0005 => state => Instruction.SUB_Vx_Vy(state),
+					0x0006 => state => Instruction.SHR_Vx_Vy(state),
+					0x0007 => state => Instruction.SUBN_Vx_Vy(state),
+					0x000E => state => Instruction.SHL_Vx_Vy(state),
+					_ => state => Instruction.SYS_addr(state)
 				},
-				0x9000 => Instruction.SNE_Vx_Vy,
-				0xA000 => Instruction.LD_I_addr,
-				0xB000 => Instruction.JP_V0_addr,
-				0xC000 => Instruction.RND_Vx_byte,
-				0xD000 => Instruction.DRW_Vx_Vy_nibble,
+				0x9000 => state => Instruction.SNE_Vx_Vy(state),
+				0xA000 => state => Instruction.LD_I_addr(state),
+				0xB000 => state => Instruction.JP_V0_addr(state),
+				0xC000 => state => Instruction.RND_Vx_byte(state),
+				0xD000 => state => Instruction.DRW_Vx_Vy_nibble(state),
 				0xE000 => (encodedInstruction & 0x00FF) switch
 				{
-					0x009E => Instruction.SKP_Vx,
-					0x00A1 => Instruction.SKNP_Vx,
-					_ => Instruction.SYS_addr
+					0x009E => state => Instruction.SKP_Vx(state),
+					0x00A1 => state => Instruction.SKNP_Vx(state),
+					_ => state => Instruction.SYS_addr(state)
 				},
 				0xF000 => (encodedInstruction & 0x00FF) switch
 				{
-					0x0007 => Instruction.LD_Vx_DT,
-					0x000A => Instruction.LD_Vx_K,
-					0x0015 => Instruction.LD_DT_Vx,
-					0x0018 => Instruction.LD_ST_Vx,
-					0x001E => Instruction.ADD_I_Vx,
-					0x0029 => Instruction.LD_F_Vx,
-					0x0033 => Instruction.LD_B_Vx,
-					0x0055 => Instruction.LD_I_Vx,
-					0x0065 => Instruction.LD_Vx_I,
-					_ => Instruction.SYS_addr
+					0x0007 => state => Instruction.LD_Vx_DT(state),
+					0x000A => state => Instruction.LD_Vx_K(state),
+					0x0015 => state => Instruction.LD_DT_Vx(state),
+					0x0018 => state => Instruction.LD_ST_Vx(state),
+					0x001E => state => Instruction.ADD_I_Vx(state),
+					0x0029 => state => Instruction.LD_F_Vx(state),
+					0x0033 => state => Instruction.LD_B_Vx(state),
+					0x0055 => state => Instruction.LD_I_Vx(state),
+					0x0065 => state => Instruction.LD_Vx_I(state),
+					_ => state => Instruction.SYS_addr(state)
 				},
-				_ => Instruction.SYS_addr
+				_ => state => Instruction.SYS_addr(state)
 			};
 		}
 	}
